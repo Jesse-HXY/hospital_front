3272ea776de14dfe18f672d26ac01ad7ebd358ed
songqingyu <1297889314@qq.com>
2019-05-28 15:24:03 +0800

    department vue 和 app.vue更新

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,90 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container style="height: 750px; border: 1px solid #eee">
+      <el-aside style="width:15%">
+        <el-menu :default-openeds="['1', '6']">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-setting"></i>基础信息维护</template>
+            <el-menu-item index="1-1">常数类别管理</el-menu-item>
+            <el-menu-item index="1-2">科室管理</el-menu-item>
+            <el-menu-item index="1-3">挂号级别管理</el-menu-item>
+            <el-menu-item index="1-4">诊断目录管理</el-menu-item>
+            <el-menu-item index="1-5">非药品收费项目管理</el-menu-item>
+            <el-menu-item-group>
+              <template slot="title">医生排班管理</template>
+              <el-menu-item index="1-6">生成排班规则</el-menu-item>
+              <el-menu-item index="1-7">生成排班计划</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-setting"></i>门诊挂号收费</template>
+            <el-menu-item index="2-1">现场挂号</el-menu-item>
+            <el-menu-item index="2-2">退号</el-menu-item>
+            <el-menu-item index="2-3">收费</el-menu-item>
+            <el-menu-item index="2-4">退费</el-menu-item>
+            <el-menu-item index="2-5">发票补打</el-menu-item>
+            <el-menu-item index="2-6">发票重打</el-menu-item>
+            <el-menu-item index="2-7">患者费用查询</el-menu-item>
+            <el-menu-item index="2-8">收费员日结</el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"><i class="el-icon-setting"></i>门诊医生工作站</template>
+            <el-menu-item index="3-1">门诊病历首页</el-menu-item>
+            <el-menu-item index="3-2">检查申请</el-menu-item>
+            <el-menu-item index="3-3">检验申请</el-menu-item>
+            <el-menu-item index="3-4">门诊确诊</el-menu-item>
+            <el-menu-item index="3-5">处置申请</el-menu-item>
+            <el-menu-item index="3-6">成药处方</el-menu-item>
+            <el-menu-item index="3-7">草药处方</el-menu-item>
+            <el-menu-item index="3-8">诊毕</el-menu-item>
+            <el-menu-item index="3-9">患者费用明细查询</el-menu-item>
+            <el-menu-item index="3-10">医技模板管理</el-menu-item>
+            <el-menu-item index="3-11">西药处方模板管理</el-menu-item>
+            <el-menu-item index="3-12">中药处方模板管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title"><i class="el-icon-setting"></i>门诊医技工作站</template>
+            <el-menu-item index="4-1">患者检查</el-menu-item>
+            <el-menu-item index="4-2">患者检验</el-menu-item>
+            <el-menu-item index="4-3">患者处置</el-menu-item>
+            <el-menu-item index="4-4">医技管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title"><i class="el-icon-setting"></i>门诊药房工作站</template>
+            <el-menu-item index="5-1">门诊发药</el-menu-item>
+            <el-menu-item index="5-2">门诊退药</el-menu-item>
+            <el-menu-item index="5-3">药品管理</el-menu-item>
+          </el-submenu>
+          <el-submenu index="6">
+            <template slot="title"><i class="el-icon-setting"></i>门诊财务管理</template>
+            <el-menu-item index="6-1">费用科目管理</el-menu-item>
+            <el-menu-item index="6-2">门诊日结核对</el-menu-item>
+            <el-menu-item index="6-3">门诊科室工作量统计</el-menu-item>
+            <el-menu-item index="6-4">门诊医生工作量统计</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <router-view></router-view>
+    </el-container>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
 
 <script>
 export default {
--- src/components/department.vue
+++ src/components/department.vue
@@ -1,157 +1,151 @@
+<script src="../../src/js/pinyin.js"></script>
 <template>
 
-  <el-container>
-    <el-header>
-      <template>
-        科室编号：
-        <div style="width: 10%;display: inline-block">
-          <el-input
-            v-model="searchdId"
-            size="mini"
-            placeholder="输入科室编号"
-          />
-        </div>
-        &nbsp&nbsp&nbsp&nbsp科室名称：
-        <div style="width: 10%;display: inline-block">
-          <el-input
-            v-model="searchdName"
-            size="mini"
-            placeholder="输入科室名称"
-          />
-        </div>
-        &nbsp&nbsp&nbsp&nbsp科室类别：
-        <div style="width: 10%;display: inline-block">
-          <el-input
-            v-model="searchdType"
-            size="mini"
-            placeholder="输入科室类别"
-          /></div>
-        &nbsp&nbsp&nbsp&nbsp科室分类：
-        <div style="width: 10%;display: inline-block">
-          <el-input
-            v-model="searchdCategory"
-            size="mini"
-            placeholder="输入科室分类"
-          />
-        </div>
-      </template>
-      &nbsp&nbsp&nbsp&nbsp
-      <el-button width="100" @click="onTapSearch">查询</el-button>
-      <el-button width="100"  @click="dialogFormVisible = true">增加</el-button>
+  <el-container style="height: 500px; border: 1px solid #eee">
 
 
-    </el-header>
-    <el-aside></el-aside>
-    <el-main>
-      <el-dialog title="添加科室" :visible.sync="dialogFormVisible" width="350px" >
-        <el-form>
-          <el-form-item label="科室名称" :label-width="formLabelWidth">
-            <el-input v-model="dName" autocomplete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="科室类型" :label-width="formLabelWidth">
-            <el-input v-model="dType" autocomplete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="科室类别" :label-width="formLabelWidth" >
-            <el-select v-model="dCategory"   placeholder="请选择活动区域">
-              <el-option label="医技" value="医技"></el-option>
-              <el-option label="临床" value="临床"></el-option>
-            </el-select>
-          </el-form-item>
+    <el-container>
+      <el-header style="text-align: left; font-size: 12px">
+        <template>
+          科室编号：
+          <div style="width: 200px;display: inline-block">
+            <el-input
+              v-model="searchdId"
+              size="mini"
+              placeholder="输入科室编号"
+            />
+          </div>
+          &nbsp&nbsp&nbsp&nbsp科室名称：
+          <div style="width: 200px;display: inline-block">
+            <el-input
+              v-model="searchdName"
+              size="mini"
+              placeholder="输入科室名称"
+            />
+          </div>
+          &nbsp&nbsp&nbsp&nbsp科室分类：
+          <div style="width: 200px;display: inline-block">
+            <el-input
+              v-model="searchdType"
+              size="mini"
+              placeholder="输入科室分类"
+            />
+          </div>
+          &nbsp&nbsp&nbsp&nbsp科室类型：
+          <div style="width: 200px;display: inline-block">
+            <el-input
+              v-model="searchdCategory"
+              size="mini"
+              placeholder="输入科室类别"
+            /></div>
+        </template>
+        &nbsp&nbsp&nbsp&nbsp
+        <el-button width="100" @click="onTapSearch">查询</el-button>
+        <el-button width="100"  @click="dialogFormVisible = true">增加</el-button>
 
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">取 消</el-button>
-          <el-button type="primary" @click="onTapAdd" >确 定</el-button>
-        </div>
-      </el-dialog>
-      <el-table
-        :data="departmentList"
-        stripe
-        style="width: 100%">
-        <!--<el-table-column-->
-        <!--align="right">-->
-        <!--<template slot="header" slot-scope="scope">-->
-        <!--<el-button width="100" @click="onTapSearch">查询</el-button>-->
-        <!--</template>-->
-        <!--</el-table-column>-->
+      </el-header>
 
-        <el-table-column
-          label="科室编号"
-          width="300">
-          <template slot-scope="scope">
-            <!--<span v-if="scope.$index == editIndex"  style="margin-left: 10px"><el-input v-model="dId"></el-input></span>-->
-            <span style="margin-left: 10px">{{scope.row.dId}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="科室名称"
-          width="300">
-          <template slot-scope="scope">
-            <!--<span v-if="scope.$index == editIndex"  style="margin-left: 10px"><el-input v-model="dName"></el-input></span>-->
-            <span  style="margin-left: 10px">{{scope.row.dName}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="科室分类"
-          width="300">
-          <template slot-scope="scope">
+      <el-main>
+        <el-dialog title="添加科室" :visible.sync="dialogFormVisible" width="350px" >
+          <el-form>
+            <el-form-item label="科室名称" :label-width="formLabelWidth">
+              <el-input v-model="dName" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="科室分类" :label-width="formLabelWidth">
+              <el-input v-model="dType" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="科室类型" :label-width="formLabelWidth" >
+              <el-select v-model="dCategory"   placeholder="请选择活动区域">
+                <el-option label="医技" value="医技"></el-option>
+                <el-option label="临床" value="临床"></el-option>
+              </el-select>
+            </el-form-item>
+
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="onTapAdd" >确 定</el-button>
+          </div>
+        </el-dialog>
+        <el-table
+          :data="departmentList"
+          stripe
+          style="width: 100%">
+          <!--<el-table-column-->
+          <!--align="right">-->
+          <!--<template slot="header" slot-scope="scope">-->
+          <!--<el-button width="100" @click="onTapSearch">查询</el-button>-->
+          <!--</template>-->
+          <!--</el-table-column>-->
+
+          <el-table-column
+            label="科室编号"
+            width="150">
+            <template slot-scope="scope">
+              <!--<span v-if="scope.$index == editIndex"  style="margin-left: 10px"><el-input v-model="dId"></el-input></span>-->
+              <span style="margin-left: 10px">{{scope.row.dId}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="科室名称"
+            width="300">
+            <template slot-scope="scope">
+              <!--<span v-if="scope.$index == editIndex"  style="margin-left: 10px"><el-input v-model="dName"></el-input></span>-->
+              <span  style="margin-left: 10px">{{scope.row.dName}}</span>
+            </template>
+          </el-table-column>
+
+          <el-table-column
+            label="科室分类"
+          width="200">
+            <template slot-scope="scope">
+              <span v-if="scope.$index == editIndex"  style="margin-left: 10px"><el-input  v-model="dType"></el-input></span>
+              <span v-else style="margin-left: 10px">{{scope.row.dType}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="科室类型"
+            width="200">
+            <template slot-scope="scope">
                 <span v-if="scope.$index == editIndex"  style="margin-left: 10px"><el-select v-model="dCategory"  placeholder="请选择活动区域">
               <el-option label="医技" value="医技"></el-option>
               <el-option label="临床" value="临床"></el-option>
             </el-select></span>
-            <span  v-else style="margin-left: 10px">{{scope.row.dCategory}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="科室类别">
-          <template slot-scope="scope">
-            <span v-if="scope.$index == editIndex"  style="margin-left: 10px"><el-input  v-model="dType"></el-input></span>
-            <span v-else style="margin-left: 10px">{{scope.row.dType}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          align="right">
-          <!--<template slot="header" slot-scope="scope">-->
-          <!--<el-button width="100" @click="onTapSearch">查询</el-button>-->
-          <!--</template>-->
-          <template slot-scope="scope">
-            <el-button
-              size="mini"
-              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-            <el-button
-              size="mini"
-              @click="handleAdd(scope.$index, scope.row)" v-if="scope.$index == editIndex">添加</el-button>
-            <el-button
-              size="mini" v-else disabled>添加</el-button>
-            <el-button
-              size="mini"
-              type="danger"
-              @click="onTapGiveUp" v-if="scope.$index == editIndex">放弃</el-button>
-            <el-button
-              size="mini"
-              type="danger"
-              @click="handleDelete(scope.$index, scope.row)" v-else>删除</el-button>
+              <span  v-else style="margin-left: 10px">{{scope.row.dCategory}}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            align="right">
+            <!--<template slot="header" slot-scope="scope">-->
+            <!--<el-button width="100" @click="onTapSearch">查询</el-button>-->
+            <!--</template>-->
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+              <el-button
+                size="mini"
+                @click="handleAdd(scope.$index, scope.row)" v-if="scope.$index == editIndex">添加</el-button>
+              <el-button
+                size="mini" v-else disabled>添加</el-button>
+              <el-button
+                size="mini"
+                type="danger"
+                @click="onTapGiveUp" v-if="scope.$index == editIndex">放弃</el-button>
+              <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)" v-else>删除</el-button>
 
-          </template>
-        </el-table-column>
-      </el-table>
-    </el-main>
-    <el-footer></el-footer>
-
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
   </el-container>
+
+
 </template>
-
-<style>
-  .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
-  }
-
-  .el-aside {
-    color: #333;
-  }
-</style>
 
 <script>
   import{getPinYinFirstCharacter} from '../js/pinyin'
@@ -167,6 +161,7 @@
       let editList=[];
       let editIndex = -1;
 
+
       return {
         departmentList:departmentList,
         dId:dId,
@@ -176,8 +171,9 @@
         editIndex:editIndex,
         searchdId : '',
         searchdName :'',
-        searchdCategory :'',
+
         searchdType : "",
+        searchdCategory :'',
         formLabelWidth: '120px',
         dialogFormVisible:false,
 
@@ -263,12 +259,13 @@
           data:{
             dId:that.searchdId,
             dName:that.searchdName,
+            dType:that.searchdType,
             dCategory:that.searchdCategory,
-            dType:that.dType
+
           }
 
         }).then(response => {
-          that.departmentList = response.data
+          that.departmentList = response.data;
           console.log(JSON.stringify(response.data))
         }).catch(err=>{
           console.log(err)
@@ -298,6 +295,7 @@
         this.dialogFormVisible = false
         let that= this;
         this.dId = this.getDId();
+        console.log()
         this.$axios({
           url:"department/insertDepartment",
           method:"post",
@@ -305,10 +303,17 @@
             dId: that.dId,
             dName:that.dName,
             dType:that.dType,
-            dCategory:that.dCategory
+            dCategory:that.dCategory,
           }
         }).then(response=>{
-          this.departmentList.push(response.data.department)
+          let department={
+            dId: that.dId,
+            dName:that.dName,
+            dType:that.dType,
+            dCategory:that.dCategory,
+          };
+          console.log(response.data);
+          this.departmentList.push(department);
           that.reSet()
         }).catch(err=>{
           console.log(err)
@@ -319,10 +324,32 @@
       },
       reSet:function () {
         this.dId = ""
-        this.dCategory = ""
         this.dName = ""
         this.dType = ""
+        this.dCategory = ""
       }
+
+
+
     }
   }
+
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
